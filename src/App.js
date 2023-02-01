b790668c3b40c60a91db4ee740bcd3500cf7ae23
chequanghuyca <chequanghuybtt@gmail.com>
2023-02-01 12:43:17 +0700

    2.1/2/2023

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Topbar from './components/topbar/Topbar';
-import Homepage from './pages/homepage/Homepage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <Switch>
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
-        <Route path='/posts'>
-          <Homepage />
-        </Route>
-      </Switch>
-    </Router>
+    <div className='App'>
+      dsadas
+    </div>
   );
 }
 
