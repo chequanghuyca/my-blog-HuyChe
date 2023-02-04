704d02b9ef20af9355d78570344880a1ccc9d5e2
chequanghuyca <chequanghuybtt@gmail.com>
2023-02-05 00:02:16 +0700

    small fix

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -8,23 +8,23 @@ export default function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='sidebar-item'> 
-        <span className='sidebar-title'>ABOUT ME</span>
+        <span className='sidebar-title'>MÌNH LÀ HUY CHE</span>
         <img src='https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/328435903_759384045109056_472057914952073295_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=4fOXtRT3aRwAX8kl8Gd&_nc_ht=scontent.fsgn2-3.fna&oh=00_AfDb1bs-Vum41TKacqpeGoiYt14cRdwx8n5ZqbsY-csI3w&oe=63DE6F46' alt='' />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        <p>If you're trying to achieve, there will be roadblocks. I've had them, everybody has had them. But obstacles don't have to stop you. If you run into a wall, don't turn around and give up. Figure out how to climb it, go through it, or work around it.</p>
       </div>
       <div className='sidebar-item'> 
-        <span className='sidebar-title'>CATEGORIES</span>
+        <span className='sidebar-title'>NHỮNG THỨ VỀ HUY CHE</span>
         <ul className='sidebar-list'> 
-          <li className='sidebar-list-item'>Life</li>
-          <li className='sidebar-list-item'>Music</li>
-          <li className='sidebar-list-item'>Style</li>
-          <li className='sidebar-list-item'>Sport</li>
-          <li className='sidebar-list-item'>Tech</li>
-          <li className='sidebar-list-item'>Cinema</li>
+          <li className='sidebar-list-item'>Cuộc sống</li>
+          <li className='sidebar-list-item'>Âm nhạc</li>
+          <li className='sidebar-list-item'>Phong cách</li>
+          <li className='sidebar-list-item'>Thể thao</li>
+          <li className='sidebar-list-item'>Công nghệ</li>
+          <li className='sidebar-list-item'>Phim</li>
         </ul>
       </div>
       <div className='sidebar-item'>
-        <span className='sidebar-title'>FOLLOW US</span>
+        <span className='sidebar-title'>LIÊN HỆ VỚI HUY CHE</span>
         <div className='sidebar-social'>
           <MyLink href='https://www.facebook.com/huycheeeeee' target='_blank'>
             <i className='top-icon fa fa-facebook-official' style={{color:'#238ae6'}} aria-hidden='true'/>
--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -29,13 +29,12 @@ export default function TopBar() {
                         <img className='top-img' src='https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-6/285845860_3237378019807655_7858648806545589602_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=fjWHpef9X7AAX8L-Prh&tn=qjky9nfMM4cr9NG1&_nc_ht=scontent.fsgn2-4.fna&oh=00_AfAFx2lAETdu59bL2N8CLa5Iya9fv9j3J3lW8RCa4OHR2g&oe=63DFC956' alt=''/>
                     ) : (
                         <ul className='top-list'>
-                            <li className='top-list-item'>
+                            <li className='top-list-item-right'>
                                 <Link className='link' to='/login'>Đăng nhập </Link>
                             </li>
-                            <li className='top-list-item'>
+                            <li className='top-list-item-right'>
                                 <Link className='link' to='/register'>Đăng ký </Link>
                             </li>
-                            <div className='dot'/>
                         </ul>
                     )
                 }
