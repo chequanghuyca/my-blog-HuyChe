a6d50d5c544607dfa30eefba6d4070263ae29c95
chequanghuyca <chequanghuybtt@gmail.com>
2023-02-05 16:39:52 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -30,4 +30,6 @@ app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/categories', categoryRoute);
 
-app.listen('5000', () => { console.log('Backend is running.'); });
+let port = process.env.PORT || 5000;
+
+app.listen(port, () => { console.log('Backend is running.'); });
