4e1781963a9b154d81f0ea0bb69b18c012a29a34
chequanghuyca <chequanghuybtt@gmail.com>
2023-02-05 16:00:33 +0700

    small fix

--- api/index.js
+++ api/index.js
@@ -17,24 +17,17 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL, () => { console.log('Connected to MongoDB'); });
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
-  app.post('/api/upload', upload.single('file'), (req, res) => {
-    res.status(200).json('File has been uploaded');
-  });
-  
-  app.use('/api/auth', authRoute);
-  app.use('/api/users', userRoute);
-  app.use('/api/posts', postRoute);
-  app.use('/api/categories', categoryRoute);
-  
-  app.listen('5000', () => {
-    console.log('Backend is running.');
-  });
+  destination: (req, file, cb) => { cb(null, 'images'); },
+  filename: (req, file, cb) => { cb(null, req.body.name); }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => { res.status(200).json('File has been uploaded'); });
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
+
+app.listen('5000', () => { console.log('Backend is running.'); });
